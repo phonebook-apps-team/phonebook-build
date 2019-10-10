7d899414cb9a0cee0a183fb4cff7510a57e23437
hardyzulhardiyanto <azulhardiyanto@gmail.com>
2019-10-11 00:42:22 +0700

    action reducer modify template

---
+++ client/src/action/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const API_URL = 'localhost:3001/api/phonebooks/'
+
+const request =  axios.create({
+    baseUrl: API_URL,
+    timeout: 1000
+});
+
+// start load ItemList from database
+export const loadItemDataSuccess = (store) => ({
+    type: 'LOAD_ITEM_SUCCESS',
+    store
+  })
+  
+  export const loadItemDataFailure = () => ({
+    type: 'LOAD_ITEM_FAILURE'
+  })
+  
+  export const LoadItem = () => {
+    return dispatch => {
+      return request.get('store')
+      .then(function (response) {
+        dispatch(loadItemDataSuccess(response.data))
+      })
+      .catch(function (error) {
+        console.error(error);
+        dispatch(loadItemDataFailure())
+      });
+    }
+  }
+  
+  // end load item data
